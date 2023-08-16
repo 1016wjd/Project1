9f905423618a98254f2abfef8456a92f68d05342
KimHyunJung <hday10166@gamil.com>
2023-08-16 17:25:07 +0900

    네번째 커밋

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def mul_func(n1, n2):
 def div_func(n1, n2):
     return n1/n2
 
+def squ_func(n1, n2):
+    return n1**n2
 ## 전역 변수부
 num1, num2 = 100, 200
 result = 0
@@ -28,3 +30,6 @@ print(num1, '*' ,num2, '=' ,result)
 
 result = div_func(num1, num2)
 print(num1, '/' ,num2, '=' ,result)
+
+result = squ_func_func(num1, num2)
+print(num1, '**' ,num2, '=' ,result)
